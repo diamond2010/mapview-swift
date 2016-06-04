30b6a2486016b800a756e7f3b43e1d5964a45ca3
Thorsten Herbst <herbst.thorsten@icloud.com>
2016-06-04 14:39:11 +0200

    fix issue of not showing current user location as a blue dot

--- mapview/TH_ViewController.swift
+++ mapview/TH_ViewController.swift
@@ -24,7 +24,7 @@ class TH_ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDe
     
     
     private struct Constants {
-        static let distance:CLLocationDistance = 500
+        static let distance:CLLocationDistance = 1000
         static let latitudeDelta:Double = 0.112872
         static let longitudeDelta:Double = 0.109863
         static let annotationIdentifier = "annotation"
@@ -39,21 +39,34 @@ class TH_ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDe
         var distance:CLLocationDistance = Constants.distance
         if usersLocation != nil{
             var region:MKCoordinateRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: usersLocation.coordinate.latitude, longitude: usersLocation.coordinate.longitude), span: MKCoordinateSpan(latitudeDelta: Constants.latitudeDelta, longitudeDelta: Constants.longitudeDelta))
-      
+            
             self.mapView.region = region;
             self.mapView.addAnnotations(places);
+            self.mapView.showsUserLocation = true
+            self.mapView.showsPointsOfInterest = true
+            self.mapView.showsBuildings = true
         }
+        
     }
+    
+    func mapView(mapView: MKMapView!, didUpdateUserLocation userLocation: MKUserLocation!) {
+        mapView.setCenterCoordinate(usersLocation.coordinate, animated: true)
+    }
+    
     override func viewDidDisappear(animated: Bool) {
         super.viewDidDisappear(animated)
         
     }
+    
     func mapView(mapView: MKMapView!, viewForAnnotation annotation: MKAnnotation!) -> MKAnnotationView! {
+        if (annotation.isKindOfClass(MKUserLocation)){
+            return nil
+        }
         var anView = mapView.dequeueReusableAnnotationViewWithIdentifier(Constants.annotationIdentifier) as?MKPinAnnotationView
         if anView == nil {
             anView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: Constants.annotationIdentifier)
             anView!.canShowCallout = true
-            anView!.pinColor = .Green
+            anView!.pinColor = .Purple
             anView!.animatesDrop = true
         }
         else {
@@ -61,6 +74,7 @@ class TH_ViewController: UIViewController,MKMapViewDelegate, CLLocationManagerDe
         }
         return anView
     }
+    
     func mapView(mapView: MKMapView!, didAddAnnotationViews views: [AnyObject]!) {
       println("annotations added")
     }
